03529095e8434d69dc731e697cb5197ace548e59
Saulius Stasiukaitis <stasiukaitis.saulius@gmail.com>
2017-07-07 07:40:55 +0200

    Fix notice: Undefined variable: sShopId
    Shop ID is used in queries but is not defined.
    Select it from Shop table.
    Sort by ASC as by default master Shop has lowest number.

--- core/fcpayone_events.php
+++ core/fcpayone_events.php
@@ -400,6 +400,8 @@ class fcpayone_events
     {
         $oDb = oxDb::getDb();
 
+        $sShopId = $oDb->getOne("SELECT OXID FROM oxshops ORDER BY OXID ASC LIMIT 1");
+
         foreach (self::$aPaymentMethods as $sPaymentOxid => $sPaymentName) {
             //INSERT PAYMENT METHOD
             self::insertRowIfNotExists('oxpayments', array('OXID' => $sPaymentOxid), "INSERT INTO oxpayments(OXID,OXACTIVE,OXDESC,OXADDSUM,OXADDSUMTYPE,OXFROMBONI,OXFROMAMOUNT,OXTOAMOUNT,OXVALDESC,OXCHECKED,OXDESC_1,OXVALDESC_1,OXDESC_2,OXVALDESC_2,OXDESC_3,OXVALDESC_3,OXLONGDESC,OXLONGDESC_1,OXLONGDESC_2,OXLONGDESC_3,OXSORT,FCPOISPAYONE,FCPOAUTHMODE,FCPOLIVEMODE) VALUES ('{$sPaymentOxid}', 0, '{$sPaymentName}', 0, 'abs', 0, 0, 1000000, '', 0, '{$sPaymentName}', '', '', '', '', '', '', '', '', '', 0, 1, 'preauthorization', 0);");

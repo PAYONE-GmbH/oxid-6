54a9922b5635fbfd4b41d25d6d6432c862df68db
andrefatchip <andre.herrmann@fatchip.de>
2018-12-04 11:35:29 +0100

    OXID-219: Moved payone order finalizing handling into own submethod

--- extend/application/models/fcPayOneOrder.php
+++ extend/application/models/fcPayOneOrder.php
@@ -338,11 +338,27 @@ class fcPayOneOrder extends fcPayOneOrder_parent
      */
     public function finalizeOrder(OxidEsales\Eshop\Application\Model\Basket $oBasket, $oUser, $blRecalculatingOrder = false)
     {
-        // Use standard method if payment type does not belong to PAYONE
-        if ($this->isPayOnePaymentType($oBasket->getPaymentId()) === false) {
-            return parent::finalizeOrder($oBasket, $oUser, $blRecalculatingOrder);
+        $iRet = parent::finalizeOrder($oBasket, $oUser, $blRecalculatingOrder);
+
+        $sPaymentId = $oBasket->getPaymentId();
+        $blPayonePayment = $this->isPayOnePaymentType($sPaymentId);
+
+        if ($blPayonePayment) {
+            $iRet = $this->_fcpoFinalizeOrder($oBasket, $oUser, $blRecalculatingOrder);
         }
 
+        return $iRet;
+    }
+
+    /**
+     * Payone handling on finalizing order
+     *
+     * @param $oBasket
+     * @param $oUser
+     * @param $blRecalculatingOrder
+     * @return bool|int
+     */
+    protected function _fcpoFinalizeOrder($oBasket, $oUser, $blRecalculatingOrder) {
         $blSaveAfterRedirect = $this->_isRedirectAfterSave();
 
         $mRet = $this->_fcpoEarlyValidation($blSaveAfterRedirect, $oBasket, $oUser, $blRecalculatingOrder);

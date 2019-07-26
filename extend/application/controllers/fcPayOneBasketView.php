360ecadb0a2708c52aa5a2c7717fdf6b63cfd6d4
andrefatchip <andre.herrmann@fatchip.de>
2019-07-26 14:47:31 +0200

    OXID-270: Fixed duplicate refnr and js-undefined problem

--- extend/application/controllers/fcPayOneBasketView.php
+++ extend/application/controllers/fcPayOneBasketView.php
@@ -99,6 +99,7 @@ class fcPayOneBasketView extends fcPayOneBasketView_parent
             $this->_oFcpoHelper->fcpoDeleteSessionVariable('fcpoAmazonWorkorderId');
             $this->_oFcpoHelper->fcpoDeleteSessionVariable('fcpoAmazonReferenceId');
             $this->_oFcpoHelper->fcpoDeleteSessionVariable('amazonRefNr');
+            $this->_oFcpoHelper->fcpoDeleteSessionVariable('fcpoRefNr');
             $this->_oFcpoHelper->fcpoDeleteSessionVariable('usr');
         }
     }

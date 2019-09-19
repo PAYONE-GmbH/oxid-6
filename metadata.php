aac63b06a22cd155d8bdd1efdbd0ffef8ea7fbdb
hreinberger <hannes.reinberger@bspayone.com>
2019-09-19 15:59:51 +0200

    version bump to 1.2.0

--- metadata.php
+++ metadata.php
@@ -34,7 +34,7 @@ $aModule = array(
                             https://www.payone.de
                         </a>',
     'thumbnail'     => 'picture.gif',
-    'version'       => '1.1.0',
+    'version'       => '1.2.0',
     'author'        => 'FATCHIP GmbH',
     'email'         => 'kontakt@fatchip.de',
     'url'           => 'https://wiki.fatchip.de/public/faqpayone',

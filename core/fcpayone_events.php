1f7de47b5125251546fbc24f820800493d1ba8c2
Vincent Boulanger <vincent@FC-Vincent.fatchip.de>
2022-12-19 12:34:09 +0100

    OX6-133: Methods renaming

--- core/fcpayone_events.php
+++ core/fcpayone_events.php
@@ -431,8 +431,8 @@ class fcpayone_events
         'fcpo_wechatpay'=> 'PAYONE WeChat Pay',
         'fcpo_apple_pay'=> 'PAYONE Apple Pay',
         'fcporp_installment' => 'PAYONE Ratepay Ratenkauf',
-        'fcpopl_secinvoice' => 'PAYONE gesicherter Rechnungskauf (neu)',
-        'fcpopl_secinstallment' => 'PAYONE gesicherter Ratenkauf',
+        'fcpopl_secinvoice' => 'PAYONE Gesicherter Rechnungskauf (neu)',
+        'fcpopl_secinstallment' => 'PAYONE Gesicherter Ratenkauf',
 
     );
 

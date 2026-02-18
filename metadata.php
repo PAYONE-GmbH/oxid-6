432bb9a900d463d6b0df84b9fa28f81f4eb374aa
Teuber <jan.teuber@worldline.com>
2026-02-18 14:24:16 +0100

    version bump to 1121

--- metadata.php
+++ metadata.php
@@ -34,7 +34,7 @@ $aModule = array(
                             https://www.payone.com
                         </a>',
     'thumbnail'     => 'picture.gif',
-    'version'       => '1.12.0',
+    'version'       => '1.12.1',
     'author'        => 'FATCHIP GmbH',
     'email'         => 'kontakt@fatchip.de',
     'url'           => 'https://wiki.fatchip.de/public/faqpayone',
